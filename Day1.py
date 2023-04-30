26f45ec90c096708adf184c883932bcdc44887fa
Sabinkc12 <sabinkcoffical@gmail.com>
2023-04-30 11:41:55 +0545

    print/input

---
+++ Day1.py
@@ -0,0 +1,10 @@
+#Today Lets Learn about Print function and input function
+
+print("Hello World ")
+# I have put Hastag so that this remain as commant 
+#this print function let us print whatever inside "Quotation Marks " 
+input("What is your name :")
+#input function accept the name string which we have enter but we need 
+#print function to display it so if we combine those two we get
+print(input("What is your name :"))
+
