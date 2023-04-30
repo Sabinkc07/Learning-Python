cac3475b31eb7b170fefa6f5f5b1366137adfdf8
Sabinkc12 <sabinkcoffical@gmail.com>
2023-04-30 14:02:50 +0545

    BMI

--- Coding Challenges/BMI Calculator.py
+++ Coding Challenges/BMI Calculator.py
@@ -1,6 +1,20 @@
-#bmi calculator
+# BMI calculator
 # BMI = Weight(kg)/height^2(m2)
-print("Welcome To BIM Calculator ")
-weight= input("Enter your weight in kg :")
-height = input("Enter your height in m:")
-BMI
+
+print("Welcome To BMI Calculator")
+
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in m: "))
+m2 = height ** 2
+BMI = weight / m2
+print("Your BMI is " + str(BMI))
+
+if BMI < 18.5:
+    print("You are Underweight")
+elif BMI >= 18.5 and BMI <= 24.9:
+    print("You have a Normal weight")
+elif BMI >= 25 and BMI <= 29.9:
+    print("You are Overweight")
+else:
+    print("You have Obesity")
+
