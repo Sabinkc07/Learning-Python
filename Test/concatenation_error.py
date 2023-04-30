27dfdf123cc1b3879cd771ab92bc8538b4f98465
Sabinkc12 <sabinkcoffical@gmail.com>
2023-04-30 13:07:21 +0545

    test

---
+++ Test/concatenation_error.py
@@ -0,0 +1,10 @@
+#To find number of character from entered name 
+name = input("Enter your name:")
+char_name = len(name)
+print("Your name have " + char_name + "Character ")
+#Traceback (most recent call last):
+ # File "/Users/sabinkc/Documents/100 days of python/Learning-Python/test.py", line 4, in <module>
+  #  print("Your name have " + char_name + "Character ")
+  #        ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~
+#ypeError: can only concatenate str (not "int") to str
+# this is the error I get 
