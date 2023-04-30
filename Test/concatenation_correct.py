34d04b81166f2c246ff0590563c2ac2a1b893d7d
Sabinkc12 <sabinkcoffical@gmail.com>
2023-04-30 13:07:25 +0545

    test

---
+++ Test/concatenation_correct.py
@@ -0,0 +1,4 @@
+name = input("Enter your name:")
+char_name = len(name)
+print("Your name have " + str(char_name)  + "Character ")
+#this code run fine because the char_name is changed into string using string function
